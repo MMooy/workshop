d8bc29ad32bd82ca329d9085e6ce340d720e7b8c
MMooy <Michielmooy95@gmail.com>
2018-02-23 10:51:58 +0100

    second commit

--- src/workshop/Main.java
+++ src/workshop/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!2");
+		System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
 	}
 }
